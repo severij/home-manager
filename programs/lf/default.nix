0483635f35711a5d5808c8f7758c6081b638a6be
Severi Jääskeläinen <severi.jaaskelainen@gmail.com>
2023-11-28 21:47:33 +0200

    More lf config

--- programs/lf/default.nix
+++ programs/lf/default.nix
@@ -3,5 +3,61 @@
   extraConfig = ''
     set previewer ${./previewer}
     set cleaner ${./cleaner}
+    set drawbox
+    set icons
+    set incfilter
+    set incsearch
+
+    map <c-n> down
+    map <c-p> up
+    map . set hidden!
+
+    cmd toggle_preview %{{
+        if [ "$lf_preview" = "true" ]; then
+            lf -remote "send $id :set preview false; set ratios 1:5"
+        else
+            lf -remote "send $id :set preview true; set ratios 1:2:3"
+        fi
+    }}
+
+    map zp toggle_preview
+
+    map f :filter; set user_filter true
+
+    cmap <enter> &{{
+        # select and open file
+        if [ "$lf_user_filter" = true ]; then
+            lf -remote "send $id :cmd-enter; set user_filter false; setfilter; open"
+        else
+            lf -remote "send $id cmd-enter"
+        fi
+    }}
+    
+    cmap <esc> &{{
+        # exit filter mode
+        if [ "$lf_user_filter" = true ]; then
+            lf -remote "send $id :cmd-escape; set user_filter false"
+        else
+            lf -remote "send $id cmd-escape"
+        fi
+    }}
+    
+    cmap <c-n> &{{
+        # go to next file
+        if [ "$lf_user_filter" = true ]; then
+            lf -remote "send $id down"
+        else
+            lf -remote "send $id cmd-history-next"
+        fi
+    }}
+    
+    cmap <c-p> &{{
+        # go to previous file
+        if [ "$lf_user_filter" = true ]; then
+            lf -remote "send $id up"
+        else
+            lf -remote "send $id cmd-history-prev"
+        fi
+    }}
   '';
 }
